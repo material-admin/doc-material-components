fc511c1c9f102593995f26cbb20f1bd571992478
Jose Alba Aguado <jaguado@google.com>
2019-10-16 10:40:46 -0400

    flutter example of a toggle button

---
+++ toggle-button-examples/Flutter-toggle/toggle_demo/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'toggle_icon_demo.dart';
+import 'toggle_bar_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toggle Button Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/toggleIconPage',
+      routes: {
+        '/toggleBarPage': (context) => ToggleBarDemo(title: 'Toggle Bar Demo'),
+        '/toggleIconPage': (context) => ToggleIconDemo(title: 'Toggle Icon Demo'),
+      },
+    );
+  }
+}
+
+
+
+
+
